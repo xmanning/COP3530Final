75c63cd20e0257852dd3d25eba7fd2f0394bade4
tashybu <natabuehler@gmail.com>
2025-04-18 01:45:02 -0400

    made the meshinator

--- include/Mesh.hpp
+++ include/Mesh.hpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <VAO.hpp>
 #include <VBO.hpp>
+#include <string>
+#include <fstream>
+#include <sstream>
 class Mesh
 {
     std::vector<glm::vec3> vertices;
@@ -10,7 +13,7 @@ class Mesh
     VBO* vbo;
     VAO* vao;
     public:
-        Mesh(std::vector<glm::vec3> vertices, std::vector<glm::vec3> normals);
+        Mesh(std::string path);
         void Render();
         std::vector<glm::vec3>& getVertices();
 };
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,9 +1,61 @@
 #include "Mesh.hpp"
 
-Mesh::Mesh(std::vector<glm::vec3> vertices, std::vector<glm::vec3> normals)
+Mesh::Mesh(std::string path)
 {
-    this->vertices = vertices;
-    this->normals = normals;
+    std::ifstream file(path);
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::vec3> normals;
+    std::vector<int> indices;
+    std::vector<int> normalIndices;
+
+    std::string buffer;
+    while(std::getline(file, buffer))
+    {
+        std::stringstream ss(buffer);
+        std::string dataType;
+        ss >> dataType;
+        if(dataType == "v")
+        {
+            float x, y, z;
+            ss >> x;
+            ss >> y;
+            ss >> z;
+            vertices.push_back({x,y,z});
+        }
+        else if(dataType == "vn")
+        {
+            float x, y, z;
+            ss >> x;
+            ss >> y;
+            ss >> z;
+            normals.push_back({x,y,z});
+        }
+        else(dataType == "f")
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                int index;
+                ss >> index;
+                indices.push_back(index);
+                ss.ignore();
+                ss >> index;
+                normalIndices.push_back(index);
+                ss.ignore();
+                ss >> index;
+            }
+        }
+    }
+    file.close();
+
+    for(int index : indices)
+    {
+        this->vertices.push_back(vertices[index]);
+    }
+    for(int index : normalIndices)
+    {
+        this->normals.push_back(normals[index]);
+    }
+
     vbo = new VBO(GL_VERTEX_ARRAY, false);
     vao = new VAO();
     vao->Attribute(vbo, 0, 3, GL_FLOAT, sizeof(float) * 6, 0);
